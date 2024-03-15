79abb96bce99e8333423d2b1757e6544d74c58af
javidaghayev <jawaghayev@gmail.com>
2024-03-15 21:03:19 +0400

    task4

---
+++ task4.py
@@ -0,0 +1,146 @@
+# # task1 
+# list1 = ["bmw", "audi", "vw", "benz"]
+# i = 0
+
+# while i < len(list1):
+#     print(list1[i])
+#     i+=1
+
+
+
+# task2 
+    
+# alphabet = ["a", "b", "c", "d", "e", "f", "g", "h"]
+# i = 0
+# while i < len(alphabet):   
+#     if alphabet[i] == "e":
+#         i+=1
+#         continue
+#     print(alphabet[i])
+#     i+=1
+
+
+
+# task3 
+
+# num = int(input("Reqem daxil edin: "))
+
+# i = 0
+
+# while i <= num:
+
+#     print(i)
+#     i+=1
+
+    
+
+
+# task4 
+
+
+# for i in range(3):
+#     password = input("Shifreni daxil edin: ")
+#     if password == "1905":
+#         print("Girish edildi")
+#         break
+#     else:
+#         print("Max 3 sehv shansiniz var")
+# else:
+#     print("Shansiniz bitti")
+
+
+
+
+
+# task5 
+
+
+# test1 = input("3*5=? a)15 b)20 c)10 ")
+# test2 = input("4*5=? a)30 b)20 c)10 ")
+# test3 = input("2*5=? a)5 b)20 c)10 ")
+# test4 = input("5*5=? a)15 b)55 c)25 ")
+# test5 = input("10*5=? a)50 b)45 c)55 ")
+# test6 = input("3*3=? a)11 b)9 c)12 ")
+# test7 = input("6*6=? a)18 b)36 c)24 ")
+# test8 = input("3*2=? a)9 b)6 c)5 ")
+# test9 = input("0*5=? a)5 b)0 c)1 ")
+# test10 = input("3*1=? a)2 b)3 c)4 ")
+
+# trueanswer = 0
+# falseanswer = 0
+
+# if test1 == "a":
+#     trueanswer += 1
+# else:
+#     falseanswer +=1
+
+# if test2 == "b":
+#     trueanswer += 1
+# else:
+#     falseanswer +=1
+
+# if test3 == "c":
+#     trueanswer += 1
+# else:
+#     falseanswer +=1
+
+# if test4 == "c":
+#     trueanswer += 1
+# else:
+#     falseanswer +=1
+
+# if test5 == "a":
+#     trueanswer += 1
+# else:
+#     falseanswer +=1
+
+# if test6 == "b":
+#     trueanswer += 1
+# else:
+#     falseanswer +=1
+
+# if test7 == "b":
+#     trueanswer += 1
+# else:
+#     falseanswer +=1
+
+# if test8 == "b":
+#     trueanswer += 1
+# else:
+#     falseanswer +=1
+
+# if test9 == "b":
+#     trueanswer += 1
+# else:
+#     falseanswer +=1
+
+# if test10 == "b":
+#     trueanswer += 1
+# else:
+#     falseanswer +=1
+
+# if falseanswer %4 == 0:
+#     trueanswer -= 1
+
+# print("Duz cavablarin sayi: ", trueanswer)
+# print("Sehv cavablarin sayi: ", falseanswer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
